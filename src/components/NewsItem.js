4dcde2fd971c5ddadb55ff614e95ffcb97509490
gokul153 <dgkgokul15@gmail.com>
2025-10-31 14:10:23 +0530

    undestanding the state

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 
 export class NewsItem extends Component {
+ 
   render() {
-    let { title, description } = this.props;
+    let { title, description ,imageurl,newsUrl} = this.props;
     return (
       <div>
-        <div className="card" style={{ width: '18rem' }}> {/* Corrected style attribute syntax */}
-          <img src="https://www.investors.com/wp-content/uploads/2019/03/stock-Paypal-08-shutter.jpg" className="card-img-top" alt="..." /> {/* Used 'className' instead of 'class' and made tag self-closing */}
+        <div className="my-3" style={{ width: '18rem' }}> {/* Corrected style attribute syntax */}
+          <img src={imageurl} className="card-img-top" alt="..." /> {/* Used 'className' instead of 'class' and made tag self-closing */}
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/newsDetail" className="btn btn-primary">Go somewhere</a>
+            <a href="/newsDetail" className="btn btn-primary">Read More</a>
           </div>
         </div>
       </div>
